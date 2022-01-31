f8c907adc21809c24d3a256f10ccf736b5ebb367
siddik <spolatde@gmail.com>
2022-01-31 10:53:11 +0100

    post and get erledigt

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import express from "express";
  import { addRecord, getAllRecords} from './controller/records.controller.js';
 
+ /* baslangicta npm init , npm install express lowdb, terminale yazilarak gerekli paketler yüklenir */
+
 const PORT = 4000;
 const app = express();
 
-app.use(express.json());
+app.use(express.json()); //bu komut olmadan posttan gelen veriyi dataya alamayiz
 
 
 app.post('/api/records', addRecord )
